e0cfefb363168ffbdaf03bcf305e25228bb89c3f
Yuki <yukipooh4120@gmail.com>
2021-04-20 01:47:21 -0700

    add to do function and validation check completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* This application covers Object-oriented programming with JavaScript */
 /* Variables */
 const itemText = document.getElementById("item");
 const priority = document.getElementById("priority");
@@ -13,60 +14,90 @@ class Item {
   }
 }
 
-//action
+//UI
 class UI {
-  //methods : display item from local storage when the page is loaded
-  static displayItem = () => {
-    //test data
-    const items = [{
-      item: "shopping",
-      priority: "medium"
-    },
-    {
-      item: "studing JavaScript",
-      priority: "high"
-    },
-    {
-      item: "laundry",
-      priority: "low"
-    }]
+  constructor() {
+    this._itemArray = [];
+  }
+  //methods 
+  static displayItem = (itemObj) => {
+
     //#1: get the data from localstorage and assign it to itemArray
-    const itemArray = items; // will be changed to getting from local storage
-    UI.createHTML(itemArray);
+    UI._itemArray = JSON.parse(localStorage.getItem("toDoItem"));
+
+    /* ================== Exception Handling ================== */
+    if ((UI._itemArray === null) && (itemObj === undefined)) {
+      //case1: when the page is first loaded and no item is stored in the localStorage
+      UI._itemArray = Object.entries({}); //store empty array into the localStorage
+      localStorage.setItem("toDoItem", JSON.stringify(UI._itemArray));
+    } else if ((UI._itemArray.length !== 0) && (itemObj === undefined)) {
+      //case2: when the page is just refresed after one or more item is added
+      UI.createHTML(UI._itemArray);
+    } else if (itemObj === undefined) {
+      //case3: when the page is just refreshed after the case1 (no item is still added)
+      UI.createHTML(UI._itemArray);
+      /* ================== Exception Handling ================== */
+    } else {
+      //Normal case
+      console.log("hihi ")
+      UI._itemArray.unshift(itemObj); // add a new item into itemArray
+      UI.createHTML(UI._itemArray);
+    }
+
   }
 
   //#2: create HTML
   static createHTML = (itemArray) => {
-    const html = itemArray.map((elem) => {
-      return `
-        <tr>
-          <th>${elem.item}</th>
-          <th>${elem.priority}</th>
-          <th><button type="button"><i class="far fa-trash-alt"></i></button></th>
-        </tr>
-        `
-    }).join("");
 
-    itemTable.innerHTML = html;
-    return itemArray;
+    //when the page is first loaded, no item is stored in the localStorage. itemArray is empty
+    if (itemArray.length !== 0) {
+      const html = itemArray.map((elem) => {
+        return `
+          <tr>
+            <th>${elem._item}</th>
+            <th>${elem._priority}</th>
+            <th><button type="button"><i class="far fa-trash-alt"></i></button></th>
+          </tr>
+          `
+      }).join("");
+      itemTable.innerHTML = html;
+      UI.storeItem(itemArray);
+    }
+
   }
 
+  //#3 store the itemArray to localStorage
+  static storeItem = (itemArray) => {
+    localStorage.setItem("toDoItem", JSON.stringify(itemArray));
+    console.log(localStorage);
+  }
 }
 
 //When the window is loaded - > display current to do items
-document.addEventListener("DOMContentLoaded", UI.displayItem);
+document.addEventListener("DOMContentLoaded", UI.displayItem());
 
 //Add button - > display to do items
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
+  //validation check for user input
+  if ((itemText.value === "") || (priority.value === "")) {
+    alert("Input both to do item and priority");
+    //clear input
+    itemText.value = "";
+    priority.value = "";
+    return false;
+  }
   //instantiate item
   const item = new Item(itemText.value, priority.value);
-  console.log(item)
+
+  //add an item to itemArray
+  UI.displayItem(item);
+
+  //clear input
+  itemText.value = "";
+  priority.value = "";
 
 });
-
-//store items
-//localStorage.setItem("toDoItem", JSON.stringify(itemArray));
 
 //Delete button - > delete the row
