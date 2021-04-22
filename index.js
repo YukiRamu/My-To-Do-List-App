ff9e84ef438a0f0f52ee9b425074d0708236d4c2
Yuki <yukipooh4120@gmail.com>
2021-04-22 11:05:57 -0700

    fixing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /* Variables */
 const itemText = document.getElementById("item");
 const priority = document.getElementById("priority");
-const addBtn = document.querySelector(".add");
+const addBtn = document.getElementById("add");
 const itemTable = document.getElementById("itemTable");
 $('.btn').button();
 
